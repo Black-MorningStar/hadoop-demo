f4173163aaf8ddccb6f8ee36d8a098b98dccc9f7
pengshaoxiang <pengshaoxiang@bilibili.com>
2025-04-20 16:46:53 +0800

    完成MR的TopN示例

--- mapreduce/src/main/java/com/demo/Main.java
+++ mapreduce/src/main/java/com/demo/Main.java
@@ -1,7 +1,17 @@
 package com.demo;
 
+import org.apache.hadoop.fs.Path;
+
+import java.net.URI;
+
 public class Main {
     public static void main(String[] args) {
+        Path path = new Path("/data/result/demo.txt");
+        System.out.println(path.getName());
+        System.out.println(path.toString());
+        URI uri = path.toUri();
+        System.out.println(uri.getPath());
+        System.out.println(uri.toString());
         System.out.println("Hello world!");
     }
 }
